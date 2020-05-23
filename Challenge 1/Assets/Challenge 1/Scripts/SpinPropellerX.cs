47afe55c4f72d0f9d87e2efb9d4acbf251cd4bed
Husain0007 <syedhusainmustafa@gmail.com>
2020-05-24 02:59:30 +0530

    Plane Programming

---
+++ Challenge 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    //public GameObject plane;
+
+    private float spinSpeed = 4000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spin the propeller about the Z-axis
+        //transform.Rotate(Vector3.forward, spinSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.forward, spinSpeed * Time.deltaTime);
+
+    }
+}
