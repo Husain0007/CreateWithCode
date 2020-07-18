ba2c34f6ba486bdc3c70f83fd43e10519305ed77
Husain0007 <syedhusainmustafa@gmail.com>
2020-07-18 23:41:12 +0530

    2D Whack-a-mole like prototype

---
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
+
+    // Start is called before the first frame update
+
+    public void Start()
+    {
+        
+    }
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        UpdateScore(0);
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        spawnRate /= difficulty;
+        //scoreText.text = "Score: " + score;
+
+        titleScreen.gameObject.SetActive(false);
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
